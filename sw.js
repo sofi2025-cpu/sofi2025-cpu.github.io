a7b2a050f2a10e1591a04c869e78767124eb164f
Sofi <sofiya.p.78@bk.ru>
2025-02-26 00:12:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -33,8 +33,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "sofijacom.github.io",
-  "sofijacom.github.io",
+  "huangxuan.me",
+  "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
